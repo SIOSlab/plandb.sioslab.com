1f571cc5f2a9bdc5902ca38208a29e97787c9f9b
Dmitry Savransky <dsavransky@gmail.com>
2019-01-09 14:55:26 -0800

    Update about.php

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ include "templates/headerclose.php";
 <h2> Acknowledgements and Data Use Policies </h2>
 
 <p>
-The imaging mission database was created by Dmitry Savransky and is maintained by <a href='https://sioslab.com'>SIOSlab</a> at Cornell University.</p>
+The imaging mission database was created by Dmitry Savransky with contributions by Nathaniel Kinzly, Daniel Garrett and Dean Keithly, and is maintained by <a href='https://sioslab.com'>SIOSlab</a> at Cornell University.</p>
   
 
 <p><strong>
